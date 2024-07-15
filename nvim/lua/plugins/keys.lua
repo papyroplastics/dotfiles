65072432ec49c8a1d129c3d01a03064025cb0631
Lucas Llort <lucasllort@gmail.com>
2024-07-14 21:28:33 -0400

    update which-key

--- nvim/lua/plugins/keys.lua
+++ nvim/lua/plugins/keys.lua
@@ -3,23 +3,33 @@ return {
         "folke/which-key.nvim",
         event = "VeryLazy",
         version = "*",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
             local wk = require("which-key")
-            wk.setup()
-
-            wk.register(
-                {
-                    h = { name = "harpoon" },
-                    b = { name = "buffer" },
-                    t = { name = "tabs" },
-                    l = { name = "lsp" },
-                    d = { name = "debug" },
-                    dw = { name = "widgets" },
-                    ["<leader>"] = { name = "telescope" },
-                }, {
-                    prefix = "<leader>"
+            wk.setup({
+                icons = {
+                    rules = false,
+                    color = false
                 }
-            )
+            })
+            wk.add({
+                { "<leader>h", group = "harpoon" },
+                { "<leader>b", group = "buffes" },
+                { "<leader>t", group = "tabs" },
+                { "<leader>l", group = "lsp" },
+                { "<leader><leader>", group= "harpoon" },
+
+                { "<leader>0", hidden = true },
+                { "<leader>1", hidden = true },
+                { "<leader>2", hidden = true },
+                { "<leader>3", hidden = true },
+                { "<leader>4", hidden = true },
+                { "<leader>5", hidden = true },
+                { "<leader>6", hidden = true },
+                { "<leader>7", hidden = true },
+                { "<leader>8", hidden = true },
+                { "<leader>9", hidden = true },
+            })
         end,
     },
 
--- nvim/lua/plugins/movement.lua
+++ nvim/lua/plugins/movement.lua
@@ -36,7 +36,7 @@ return {
             Harpoon:setup()
         end,
         keys = {
-            {"<leader>hl", function() Harpoon.ui:toggle_quick_menu(Harpoon:list()) end, desc = "List hooks"},
+            {"<leader>hl", function() Harpoon.ui:toggle_quick_menu(Harpoon:list()) end, desc = "List hooks", group = ""},
 
             {"<leader>ha", function() Harpoon:list():add() end,    desc = "Add hook"},
             {"<leader>hr", function() Harpoon:list():remove() end, desc = "Remove hook"},
@@ -46,16 +46,16 @@ return {
             {"<tab>",      function() Harpoon:list():prev({ui_nav_wrap = true}) end, desc = "Previous hook"},
             {"<s-tab>",    function() Harpoon:list():next({ui_nav_wrap = true}) end, desc = "Next hook"},
 
-            {"<leader>0", function() Harpoon:list():select(0) end, desc = "which_key_ignore"},
-            {"<leader>1", function() Harpoon:list():select(1) end, desc = "which_key_ignore"},
-            {"<leader>2", function() Harpoon:list():select(2) end, desc = "which_key_ignore"},
-            {"<leader>3", function() Harpoon:list():select(3) end, desc = "which_key_ignore"},
-            {"<leader>4", function() Harpoon:list():select(4) end, desc = "which_key_ignore"},
-            {"<leader>5", function() Harpoon:list():select(5) end, desc = "which_key_ignore"},
-            {"<leader>6", function() Harpoon:list():select(6) end, desc = "which_key_ignore"},
-            {"<leader>7", function() Harpoon:list():select(7) end, desc = "which_key_ignore"},
-            {"<leader>8", function() Harpoon:list():select(8) end, desc = "which_key_ignore"},
-            {"<leader>9", function() Harpoon:list():select(9) end, desc = "which_key_ignore"},
+            {"<leader>0", function() Harpoon:list():select(0) end},
+            {"<leader>1", function() Harpoon:list():select(1) end},
+            {"<leader>2", function() Harpoon:list():select(2) end},
+            {"<leader>3", function() Harpoon:list():select(3) end},
+            {"<leader>4", function() Harpoon:list():select(4) end},
+            {"<leader>5", function() Harpoon:list():select(5) end},
+            {"<leader>6", function() Harpoon:list():select(6) end},
+            {"<leader>7", function() Harpoon:list():select(7) end},
+            {"<leader>8", function() Harpoon:list():select(8) end},
+            {"<leader>9", function() Harpoon:list():select(9) end},
         }
     },
 }
