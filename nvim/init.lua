08489a5e65160711605aff17a22eb5cc40601348
Lucas Llort <lucasllort@gmail.com>
2025-06-28 21:08:20 -0400

    conditional plugin loading

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,20 +5,22 @@ require('commands')
 require('filetypes')
 require('netrd')
 
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.fn.filereadable(lazypath) then
-    vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable',
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
+local configpath = vim.fn.stdpath('config')
 
-if not vim.g.vscode then
+if vim.fn.filereadable(configpath .. '/.noplugin') == 0 and not vim.g.vscode then
+    local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+    if not vim.fn.filereadable(lazypath) then
+        vim.fn.system({
+            'git',
+            'clone',
+            '--filter=blob:none',
+            'https://github.com/folke/lazy.nvim.git',
+            '--branch=stable',
+            lazypath,
+        })
+    end
+    vim.opt.rtp:prepend(lazypath)
+
     require('lazy').setup('plugins')
 end
 
